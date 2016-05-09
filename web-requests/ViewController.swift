a285b22013f0ea1a01f3bcef150b1a17ad833a8a
Bartosz Odrzywolek <bartosz.odrzywolek@gmail.com>
2016-05-10 00:30:07 +0200

    Initial Commit

---
+++ web-requests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  web-requests
+//
+//  Created by Bartosz Odrzywołek on 10/05/16.
+//  Copyright © 2016 Bartosz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
